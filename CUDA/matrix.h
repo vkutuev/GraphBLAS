582d1aa512e29b72259b02693f91dee47c17b1b8
Tim Davis <DrTimothyAldenDavis@gmail.com>
2021-10-28 11:36:12 -0500

    matrix allocators for CUDA

--- CUDA/matrix.h
+++ CUDA/matrix.h
@@ -2,9 +2,11 @@
 
 #pragma once
 
-#define chunksize 128 
+#define ASSERT(x)
 
-#define ASSERT
+//------------------------------------------------------------------------------
+// TODO: this will be in the jit code:
+#define chunksize 128 
 //#define GB_GETA( aval, ax, p) aval = (T_Z)ax[ ( p )]
 //#define GB_GETB( bval, bx, p) bval = (T_Z)bx[ ( p )]
 #define GB_ADD_F( f , s)  f = GB_ADD ( f, s ) 
@@ -12,17 +14,6 @@
 #define GB_MULTADD( c, a ,b ) GB_ADD_F( (c), GB_MULT( (a),(b) ) )
 #define GB_DOT_TERMINAL ( c )   
 //# if ( c == TERMINAL_VALUE) break;
-
-#include "GB_imin.h"
-#include "GB_zombie.h"
-
-#include "GB_opaque.h"
-
-#include "GB_nnz.h"
-#include "GB_partition.h"
-#include "GB_binary_search.h"
-#include "GB_search_for_vector_template.c"
-
 #undef GB_DOT_MERGE
 // cij += A(k,i) * B(k,j), for merge operation
 #define GB_DOT_MERGE                                                \
@@ -41,6 +32,36 @@
     }                                                               \
 }
 
+//------------------------------------------------------------------------------
+
+#include "GB_imin.h"
+#include "GB_zombie.h"
+#include "GB_opaque.h"
+#include "GB_nnz.h"
+#include "GB_partition.h"
+#include "GB_binary_search.h"
+#include "GB_search_for_vector_template.c"
+
+//------------------------------------------------------------------------------
+// remainder of this file is extracted from GraphBLAS.h:
+//------------------------------------------------------------------------------
+
+typedef struct GB_Descriptor_opaque *GrB_Descriptor ;
+typedef struct GB_Type_opaque *GrB_Type ;
+typedef struct GB_UnaryOp_opaque *GrB_UnaryOp ;
+typedef struct GB_BinaryOp_opaque *GrB_BinaryOp ;
+typedef struct GB_SelectOp_opaque *GxB_SelectOp ;
+// typedef struct GB_IndexUnaryOp_opaque *GrB_IndexUnaryOp ;    TODO
+typedef struct GB_Monoid_opaque *GrB_Monoid ;
+typedef struct GB_Semiring_opaque *GrB_Semiring ;
+typedef struct GB_Scalar_opaque *GxB_Scalar ;   // FIXME to GrB_Scalar
+typedef struct GB_Vector_opaque *GrB_Vector ;
+typedef struct GB_Matrix_opaque *GrB_Matrix ;
+
+#define GxB_HYPERSPARSE 1   // store matrix in hypersparse form
+#define GxB_SPARSE      2   // store matrix as sparse form (compressed vector)
+#define GxB_BITMAP      4   // store matrix as a bitmap
+#define GxB_FULL        8   // store matrix as full; all entries must be present
 
 typedef void (*GxB_binary_function) (void *, const void *, const void *) ;
 
